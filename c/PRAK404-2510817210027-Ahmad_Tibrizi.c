1a31ac0f945c6d3261dec9d0f33b35281602aa0d
Riez69 <ahmadbarangaja12@gmail.com>
2025-11-05 14:41:17 +0800

    update soal 5

--- c/PRAK404-2510817210027-Ahmad_Tibrizi.c
+++ c/PRAK404-2510817210027-Ahmad_Tibrizi.c
@@ -47,27 +47,27 @@ void program(){
         printf("Pilih program\n");
         for (int i = 0; i < 5; i++)
         {
-            printf("[%d]. %s\n", i, list_program[i]);
+            printf("[%d] %s\n", i+1, list_program[i]);
         }
         scanf("%d", &program_dipilih);
-        if (program_dipilih == 0)
+        if (program_dipilih == 1)
         {
             penjumlahan();
         }
-        else if (program_dipilih == 1)
+        else if (program_dipilih == 2)
         {
             pengurangan();
         }
-        else if (program_dipilih == 2)
+        else if (program_dipilih == 3)
         {
             perkalian();
 
         }
-        else if (program_dipilih == 3)
+        else if (program_dipilih == 4)
         {
             pembagian();
         }
-        else if (program_dipilih == 4)
+        else if (program_dipilih == 5)
         {
              printf("Terimakasih, telah menggunakan kalkulator Ahmad Tibrizi\n");
             break;
@@ -80,68 +80,7 @@ void program(){
     }
     }
 
-
-
-
 int main(){
     program();
     return 0;
-
 }
-
-
-
-
-// def program():
-//     while True:
-//         list_program = ['Penjumlahan', 'Pengurangan', 'Perkalian', 'Pembagian', 'exit']
-//         print('Pilih program')
-
-//         for i in range(len(list_program)):
-//             print(f'{i+1}. {list_program[i]}')
-
-//         pilihan = int(input("Masukkan Pilihan: "))
-//         if pilihan == 1:
-//             penjumlahan()
-//         elif pilihan == 2:
-//             pengurangan()
-//         elif pilihan == 3:
-//             perkalian()
-//         elif pilihan == 4:
-//             pembagian()
-//         elif pilihan == 5:
-//             print('Terimakasih, telah menggunakan kalkulator Ahmad Tibrizi')
-//             break
-//         else:
-//             print("Input anda salah, silahkan coba lagi")
-
-
-// def penjumlahan():
-//     angka1 = int(input("Masukkan nilai pertama: "))
-//     angka2 = int(input("Masukkan nilai kedua: "))
-//     hasil = angka1 + angka2
-//     print(f'Hasil penjumlahan antara {angka1:.2f} dengan {angka2:.2f} adalah {hasil:.2f}')
-
-
-// def pengurangan():
-//     angka1 = int(input("Masukkan nilai pertama: "))
-//     angka2 = int(input("Masukkan nilai kedua: "))
-//     hasil = angka1 - angka2
-//     print(f'Hasil pengurangan antara {angka1:.2f} dengan {angka2:.2f} adalah {hasil:.2f}')
-
-
-// def perkalian():
-//     angka1 = int(input("Masukkan nilai pertama: "))
-//     angka2 = int(input("Masukkan nilai kedua: "))
-//     hasil = angka1 * angka2
-//     print(f'Hasil perkalian antara {angka1:.2f} dengan {angka2:.2f} adalah {hasil:.2f}')
-
-
-// def pembagian():
-//     angka1 = int(input("Masukkan nilai pertama: "))
-//     angka2 = int(input("Masukkan nilai kedua: "))
-//     hasil = angka1 / angka2
-//     print(f'Hasil pembagian antara {angka1:.2f} dengan {angka2:.2f} adalah {hasil:.2f}')
-
-// if __name__ == "__main__":
-//     program()
